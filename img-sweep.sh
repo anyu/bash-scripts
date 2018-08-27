4ce4e070bac7699511dff019049f208b6528988f
An Yu <anyu@pivotal.io>
2018-08-27 18:33:19 -0400

    Skip non-jpeg files

--- img-sweep.sh
+++ img-sweep.sh
@@ -6,13 +6,20 @@ set -euo pipefail
 cd ~/Downloads
 
 count=0;
-for file in *; do
-  date_modified=$(date -r $file +%Y%m%d)
-  new_name=${date_modified}_img-$count.jpg
-  mv $file $new_name
 
-  echo "Renaming $file to $new_name..."
-  ((count++))
+for file in *; do
+  file_type=$(file -b $file)
+
+  if [[ $file_type == 'JPEG '* ]]; then
+    date_modified=$(date -r $file +%Y%m%d)
+    new_name=${date_modified}_img-$count.jpg
+    mv $file $new_name
+
+    echo "Renaming $file to $new_name..."
+    ((count++))
+  else
+    echo "$file is not a JPEG. Skipping rename."
+  fi
 done
 
 # Sort numerically
