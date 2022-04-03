394cc06b2e2074886be1d111249e0644304b3032
An Yu <ayu3500@gmail.com>
2022-04-03 00:03:10 -0400

    Modularize a lil

--- img-sweep.sh
+++ img-sweep.sh
@@ -4,37 +4,49 @@ set -euo pipefail
 
 NAME=${1:-img}
 
-cd "$HOME"/Downloads
-initial_item_count=$(find . -maxdepth 1 -mindepth 1 -not -path '*/\.*' | wc -l)
-num_modified=0
-new_dir=$(date +"%Y%m%d")
-mkdir $new_dir
+main() {
+  create_new_dir || true
+  rename_files || true
+}
 
-echo "Number of items in Downloads: $initial_item_count"
+create_new_dir() {
+  cd "$HOME"/Downloads
+  initial_item_count=$(find . -maxdepth 1 -mindepth 1 -not -path '*/\.*' | wc -l)
+  num_modified=0
+  new_dir=$(date +"%Y%m%d")
+  mkdir $new_dir
+}
 
-for file in *; do
-  file_type=${file##*.}
+rename_files() {
+  echo -e "\nNumber of items in Downloads: $initial_item_count"
 
-  shopt -s nocasematch
-  if [[ $file_type == 'JPEG'* ]] || [[ $file_type == 'JPG'* ]] || [[ $file_type == 'HEIC'* ]] || [[ $file_type == 'MOV'* ]]; then
-    date_modified=$(date -r "$file" +%Y%m%d)
-    new_name=${date_modified}_${NAME}-$RANDOM.${file_type}
+  for file in *; do
 
-    if [[ ! -e ~/Downloads/$new_dir/$new_name ]]; then
-      ((num_modified++))
-      echo "Renaming $file to $new_name..."
-      mv -i "$file" "$new_dir"/"$new_name"  # -i prompt as a redundant safety check
+    file_type=${file##*.}
+
+    shopt -s nocasematch
+    if [[ $file_type == 'JPEG'* ]] || [[ $file_type == 'JPG'* ]] || [[ $file_type == 'HEIC'* ]] || [[ $file_type == 'MOV'* ]]; then
+      date_modified=$(date -r "$file" +%Y%m%d)
+      new_name=2022_02_26_${NAME}-$RANDOM.${file_type}
+
+      if [[ ! -e ~/Downloads/$new_dir/$new_name ]]; then
+        ((num_modified++))
+        echo "Renaming $file to $new_name..."
+        mv -i "$file" "$new_dir"/"$new_name"  # -i prompt as a redundant safety check
+      else
+        echo "$new_name already exists. Skipping rename."
+      fi
+    shopt -u nocasematch
     else
-      echo "$new_name already exists. Skipping rename."
+      echo "$file is not a JPEG, HEIC, or MOV. Skipping rename."
+      continue
     fi
-  shopt -u nocasematch
-  else
-    echo "$file is not a JPEG, HEIC, or MOV. Skipping rename."
-  fi
-done
+  done
 
-echo -e "\\nModified $num_modified files\\n"
+  echo -e "\\nModified $num_modified files\\n"
+}
 
+main "${@}"
 
 # Considered commands:
 
